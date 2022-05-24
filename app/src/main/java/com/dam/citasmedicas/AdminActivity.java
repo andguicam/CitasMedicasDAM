a8e0af42c2bc8bf7a78845ffb76cbe29db645584
andguicam <79918281+andguicam@users.noreply.github.com>
2022-05-24 21:09:51 +0200

    Administrador añadido
    El administrador esta desarrollado, falta por probarlo en cuanto email este implementado

--- app/src/main/java/com/dam/citasmedicas/AdminActivity.java
+++ app/src/main/java/com/dam/citasmedicas/AdminActivity.java
@@ -2,24 +2,120 @@ package com.dam.citasmedicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 public class AdminActivity extends AppCompatActivity {
+    private ListView lv;
+    public List<Usuario> lista=null;
+    //private ArrayAdapter arrayAdapter;
+   // AccesoREST accesoREST;
+    public static final String direccion = "http://192.168.0.196:8080/";
 
+
+    public AdminActivity(){
+        //accesoREST=new AccesoREST();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+
+        obtenerUsuarios();
+
+        //ARRAY SACADO DE BBDD, usando AccesoREST.java. Se ponen datos en fila y subfila.
+        //Fila: Nombre usuario
+        //Subfila: DNI, tipo usuario
+        //List<Usuario> lista = obtenerUsuarios();
+
+        //Onclick: Abre la vista detallada (por hacer)
+
+
+
+//        lv.setOnItemClickListener(
+//                new OnItemClickListener()
+//                {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> arg0, View view,
+//                                            int position, long id) {
+//
+//                        //Take action here.
+//                    }
+//                }
+//        );
+
+
+
+//        lv.setOnClickListener(new AdapterView.onClickListener(){
+//            @Override
+//            public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
+//                Intent appInfo = new Intent(YourActivity.this, ApkInfoActivity.class);
+//                startActivity(appInfo);
+//            }
+//        });
+
     }
+/*    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) l.getItemAtPosition(position);
+
+        Intent intent = new Intent(this,AdminDetalle.class);
+        intent.putExtra("item",item);
+        startActivity(intent);
+        //con la posicion podemos identificar el usuario en el array.
+        //simplemente sacamos la clase usuario de ahi y la pasamos a la nueva actividad
+
+
+    }*/
+
+//    @Override
+    //al hacer tap en algun elemento de la vista, setea el texto del elemento en el nuevo fragment
+//    public void onListItemClick(ListView l, View v, int position, long id) {
+//        String user = (String) l.getItemAtPosition(position);
+        //Le pasa a la nueva actividad el intent
+        //Crea nueva actividad
+//        Intent intent = new Intent(getActivity().getApplicationContext(), DetailActivity.class);
+        //con la posicion podemos identificar el usuario en el array.
+        //simplemente sacamos la clase usuario de ahi y la pasamos a la nueva actividad
+
+        //para paso de parametros??
+//        intent.putExtra("value", item);
+//        startActivity(intent);
+        //Desde la nueva actividad se hacen las operaciones necesarias.
+//    }
+
 
     public void onAgregar(View view) {
         Intent agregarIntent = new Intent(this, AgregarUsuario.class);
         startActivity(agregarIntent);
     }
 
-    public void onEliminar(View view) {
+    /*public void onEliminar(View view) {
         Intent eliminarIntent = new Intent(this, EliminarUsuario.class);
         startActivity(eliminarIntent);
     }
@@ -27,5 +123,65 @@ public class AdminActivity extends AppCompatActivity {
     public void onModificar(View view) {
         Intent modificarIntent = new Intent(this, ModificarUsuario.class);
         startActivity(modificarIntent);
+    }*/
+
+
+    public void obtenerUsuarios() {
+
+        List<Usuario> listausers = new ArrayList<Usuario>();
+        final String URL = direccion + "obtenerUsuarios";
+        final ProgressDialog dlg = ProgressDialog.show(this,
+                "Obteniendo los datos",
+                "Por favor, espere...", true);
+
+        ArrayAdapter<String> arrayAdapter;
+        List<String> al = new ArrayList<String>();
+        setContentView(R.layout.activity_admin);
+        ListView lv = (ListView) findViewById(R.id.lista_admin);
+        Context contexto = this;
+
+        // prepare the Request
+        JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {// display response
+                        try{
+                            dlg.dismiss();
+
+                            for (int i = 0; i<response.length();i++) {
+                                JSONObject ob = response.getJSONObject(i);
+                                al.add(ob.getString("nombre") + " "+ob.getString("apellidos")+", " + ob.getString("tipo_usuario")+", "+ob.getString("dni"));
+                            }
+                            ArrayAdapter arrayAdapter = new ArrayAdapter<String>(contexto, android.R.layout.simple_list_item_1, al);
+                            lv.setAdapter(arrayAdapter);
+
+                            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    String selectedItem = (String) parent.getItemAtPosition(position);
+                                    Intent intent = new Intent(contexto,AdminDetalle.class);
+                                    intent.putExtra("item",selectedItem);
+                                    startActivity(intent);
+                                }
+                            });
+
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                            Toast.makeText(AdminActivity.this,"Error al intentar establecer contacto con el servicio",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        );
+        ColaServiciosWeb.getInstance(this).addToRequestQueue(getRequest);
+        //Singleton.getInstance(this).addToRequestQueue(getRequest);
     }
 }
--- app/src/main/java/com/dam/citasmedicas/ModificarUsuario.java
+++ app/src/main/java/com/dam/citasmedicas/ModificarUsuario.java
@@ -2,13 +2,174 @@ package com.dam.citasmedicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//TODO: representar fechas bien
 public class ModificarUsuario extends AppCompatActivity {
+    private String nombre_s;
+    private String apellidos_s;
+    private String fechaNac_s;
+    private String tipo_s;
+    private String direccion_s;
+    private String password_s;
+    private String email_s;
+    private String dni_s;
+
+    private TextView nombre;
+    private TextView apellidos;
+    private TextView fechaNac;
+    private TextView tipo;
+    private TextView direccion;
+    private TextView password;
+    private TextView email;
+    private TextView dni;
+
+    private EditText nombre_nuevo;
+    private EditText apellidos_nuevo;
+    private EditText fechaNac_nuevo;
+    private EditText tipo_nuevo;
+    private EditText direccion_nuevo;
+    private EditText password_nuevo;
+    private EditText email_nuevo;
+    private EditText dni_nuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar_usuario);
+
+        nombre_s = (String) getIntent().getExtras().get("nombre").toString();
+        apellidos_s = (String) getIntent().getExtras().get("apellidos").toString();
+        fechaNac_s = (String) getIntent().getExtras().get("fechaNac").toString();
+        password_s = (String) getIntent().getExtras().get("password").toString();
+        dni_s = (String) getIntent().getExtras().get("dni").toString();
+        direccion_s = (String) getIntent().getExtras().get("direccion").toString();
+        tipo_s = (String) getIntent().getExtras().get("tipo").toString();
+        email_s = (String) getIntent().getExtras().get("email").toString();
+
+        nombre=(TextView)findViewById(R.id.activity_agregar_nombre);
+        apellidos=(TextView) findViewById(R.id.activity_agregar_apellidos);
+        fechaNac=(TextView) findViewById(R.id.activity_agregar_fechaNac);
+        password=(TextView) findViewById(R.id.activity_agregar_password);
+        dni=(TextView) findViewById(R.id.activity_agregar_dni);
+        direccion=(TextView) findViewById(R.id.activity_agregar_direccion);
+        tipo=(TextView) findViewById(R.id.activity_agregar_tipo);
+        email=(TextView) findViewById(R.id.activity_agregar_email);
+
+        nombre_nuevo=(EditText)findViewById(R.id.activity__modificar_nuevo_nombre);
+        apellidos_nuevo=(EditText) findViewById(R.id.activity__modificar_nuevo_apellidos);
+        fechaNac_nuevo=(EditText) findViewById(R.id.activity__modificar_nuevo_fechaNac);
+        password_nuevo=(EditText) findViewById(R.id.activity__modificar_nuevo_password);
+        dni_nuevo=(EditText) findViewById(R.id.activity__modificar_nuevo_dni);
+        direccion_nuevo=(EditText) findViewById(R.id.activity__modificar_nuevo_direccion);
+        tipo_nuevo=(EditText) findViewById(R.id.activity__modificar_nuevo_tipo);
+        email_nuevo=(EditText) findViewById(R.id.activity__modificar_nuevo_email);
+
+        nombre.setText(nombre_s);
+        apellidos.setText(apellidos_s);
+        fechaNac.setText(fechaNac_s);
+        password.setText(password_s);
+        dni.setText(dni_s);
+        direccion.setText(direccion_s);
+        tipo.setText(tipo_s);
+        email.setText(email_s);
+
+        nombre_nuevo.setText(nombre_s,TextView.BufferType.EDITABLE);
+        apellidos_nuevo.setText(apellidos_s,TextView.BufferType.EDITABLE);
+        fechaNac_nuevo.setText(fechaNac_s,TextView.BufferType.EDITABLE);
+        password_nuevo.setText(password_s,TextView.BufferType.EDITABLE);
+        dni_nuevo.setText(dni_s,TextView.BufferType.EDITABLE);
+        direccion_nuevo.setText(direccion_s,TextView.BufferType.EDITABLE);
+        tipo_nuevo.setText(tipo_s,TextView.BufferType.EDITABLE);
+        email_nuevo.setText(email_s,TextView.BufferType.EDITABLE);
+
+    }
+
+
+
+    public void onConfirmar(View view) {
+        //TODO: probar este metodo por completo cuando tenga la base de datos con email actualizada
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(ModificarUsuario.this);
+        alertBuilder.setTitle("¿Modificar usuario?");
+        alertBuilder.setMessage("¿Está seguro de que desea modificar el usuario? Los datos anteriores se perderán");
+        alertBuilder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //TODO: comprobar que funciona cuando tenga el email
+                final String URL = AdminActivity.direccion + "modificarUsuario?tipo_ant="+tipo_s+"&dni_ant="+dni_s+"&dni_nuevo="+dni_nuevo.getText().toString()+"&nombre="+nombre_nuevo.getText().toString()+
+                        "&apellidos="+apellidos_nuevo.getText().toString()+"&password="+password_nuevo.getText().toString()+"&fecha="+fechaNac_nuevo.getText().toString()+"&direccion="+direccion_nuevo.getText().toString()+
+                        "&tipo="+tipo_nuevo.getText().toString();
+
+                final ProgressDialog dlg = ProgressDialog.show(ModificarUsuario.this,
+                        "Modificando el usuario",
+                        "Por favor, espere...", true);
+
+                // prepare the Request
+                StringRequest getRequest = new StringRequest(Request.Method.GET, URL,
+                        new Response.Listener<String>() {
+
+                            @Override
+                            public void onResponse(String response) {
+                                try{
+                                    dlg.dismiss();
+                                    //TODO: da fallo en la fecha, no lo pilla bien
+                                    Boolean bool = Boolean.parseBoolean(response.toString());
+                                    if (bool){
+                                        Toast.makeText(ModificarUsuario.this,"Usuario modificado correctamente",Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        Toast.makeText(ModificarUsuario.this,"Error al modificar el usuario",Toast.LENGTH_SHORT).show();
+                                    }
+                                    //Volvemos atras automaticamente, actualizando el nuevo usuario
+                                    Intent intent = new Intent(ModificarUsuario.this,AdminDetalle.class);
+                                    startActivity(intent);
+                                }
+                                catch (Exception e){
+                                    e.printStackTrace();
+                                    Toast.makeText(ModificarUsuario.this,"Error al modificar el usuario",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Error.Response", error.toString());
+                                Toast.makeText(ModificarUsuario.this,"Error al modificar el usuario",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+                ColaServiciosWeb.getInstance(ModificarUsuario.this).addToRequestQueue(getRequest);
+
+
+            }
+        });
+
+        alertBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(ModificarUsuario.this,"Modificación cancelada",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alertBuilder.show();
+
     }
 }
